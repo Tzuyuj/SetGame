537d3883716427aab614b9ef10740c2b02c9bf03
rmagaram <rmmagaram@gmail.com>
2019-01-23 13:55:48 -0500

    small changes

--- project2.rb
+++ project2.rb
@@ -1,22 +1,24 @@
 #need to make 81 unique cards; each card has a color, number, shape, and shading (+selected/not)
-#could the cards be structs? unions? or something else?
+#could the cards be structs? unions? or something else? class?
 
-#header of some kind
-int num_cards_dealt = 0;
+#header of some kind?
+num_cards_dealt = 0;
 
-while(nums_card_dealt < 12) {
+while nums_cards_dealt < 12
  #deal cards
-}
+end
 
-int user_cards = 0;
-while(user_cards < 3) {
+user_cards = 0;
+while user_cards < 3
 	puts 'Choose a card: '; #number cards 1-12? what does user type?
 	#"gets" is the command for user input
 	input = gets; #idk what kind of variable the input is here
 	#if(input matches card - but how to check?) {
 		#then select that card
 	#}
-}
+end
+
+
 =begin
 4 groups of conditions, all of which must be satisfied!
 group 1: They all have the same number or have three different numbers.
@@ -27,4 +29,5 @@ make a boolean variable for each group; test each group separately
 make elaborate if(group1 && group2 && group3 && group4) test at the end
 if set -> return "it's a set" and go back to loop of dealing cards
 if not set -> return "not a set" and deselect user's cards, go back to user selection loop
+parenthesis for multiple conditions
 =end
