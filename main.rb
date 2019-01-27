6636bfc2a22efc9a2af9bfd08682f048cf34e59b
Akul Gulrajani <gulrajani.2@osu.edu>
2019-01-27 14:05:09 -0500

    Clean up comments and add more output for readability

--- main.rb
+++ main.rb
@@ -8,7 +8,6 @@ play = true
 points = 0
 # playerOnePoints = 0
 # playerTwoPoints = 0
-#
 
 # deal cards here
 user_cards = deck_of_cards.deal(12)
@@ -19,6 +18,7 @@ card_three = nil
 # start game loops here
 while play
   valid_input = true
+  # get first card of user input
   puts "\n" + 'Choose a card from the 12 above: '
   loop do
     card_one = gets
@@ -30,6 +30,7 @@ while play
     break if valid_input
   end
   card_one = user_cards[card_one]
+  # get second card of user input
   puts 'Choose another card: '
   loop do
     card_two = gets
@@ -41,6 +42,7 @@ while play
     break if valid_input
   end
   card_two = user_cards[card_two]
+  # get third card of user input
   puts 'Choose another card'
   loop do
     card_three = gets
@@ -52,18 +54,24 @@ while play
     break if valid_input
   end
   card_three = user_cards[card_three]
+  # perform card check (the check method returns the correct third
+  # card and then compares it to the user's third card)
   if card_three == deck_of_cards.check(card_one, card_two)
+    puts 'That is a set, you get a point!'
     points += 1
     # cards pass the check, so they have to be replaced
-    # check if there are enough cards to replace them
+    # check if there are enough cards to replace them,
+    # otherwise the game ends
     if deck_of_cards.size >= 3
       user_cards[card_one] = deck_of_cards.push
       user_cards[card_two] = deck_of_cards.push
       user_cards[card_three] = deck_of_cards.push
     else
+      puts 'No more cards, left. GAME OVER!'
       play = false
     end
   else
+    puts 'That is not a set, you lose a point!'
     points -= 1
   end
   # check if no sets are possible
