cf2661a1bbf1f9a933798174fef1d52dd5fbe549
Akul Gulrajani <gulrajani.2@osu.edu>
2019-01-29 22:03:08 -0500

    Adjusted apt-get command for the setup shell script

--- gui.sh
+++ gui.sh
@@ -1,3 +1,3 @@
-sudo apt install libsdl2-dev libsdl2-image-dev libsdl2-mixer-dev libsdl2-ttf-dev
+sudo apt install autoconf bison libreadline6-dev zlib1g-dev libncurses5-dev libffi-dev libgdbm5 libgdbm-dev libsdl2-dev libsdl2-image-dev libsdl2-mixer-dev libsdl2-ttf-dev
 make && sudo make install
 gem install ruby2d
