e8d40ba931db3fcaeffb8f0d9e576317d829308c
Akul Gulrajani <gulrajani.2@osu.edu>
2019-01-29 22:27:39 -0500

    Final update (hopefully to gui.sh)

--- gui.sh
+++ gui.sh
@@ -1,6 +1,4 @@
-sudo apt install autoconf bison libreadline6-dev zlib1g-dev libncurses5-dev libffi-dev
-sudo apt install libgdbm3 libgdbm-dev libsdl2-dev libsdl2-image-dev
-sudo apt install libsdl2-mixer-dev libsdl2-ttf-dev
+sudo apt install autoconf bison libreadline6-dev zlib1g-dev libncurses5-dev libffi-dev libgdbm3 libgdbm-dev libsdl2-dev libsdl2-image-dev ibsdl2-mixer-dev libsdl2-ttf-dev
 wget "https://github.com/simple2d/simple2d/archive/v1.1.0.tar.gz"
 tar -xf v1.1.0.tar.gz
 cd simple2d-1.1.0
