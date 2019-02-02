7aebb347001765505bed6983eff4dd2d56b72f2f
Tzuyuj <zhou.2133@osu.edu>
2019-02-02 17:19:49 -0500

    buttons can be deactivated

--- table.rb
+++ table.rb
@@ -1,7 +1,10 @@
 require 'gtk2'
 require_relative 'game'
 
-
+def toggle_em(w, other_tgg)
+  # Gtk::Widget#sensitive=boolean |or #set_sensitive(boolean)
+  other_tgg.sensitive = w.active? ? false : true
+end
 # initialize window
 window = Gtk::Window.new('The Game of Set')
 window.set_default_size(700, 700)
@@ -14,36 +17,35 @@ game = Game.new
 
 
 
-
-#catImage = Gtk::Image.new("cat.jpeg")
-#event_box = Gtk::EventBox.new.add(catImage)
-#event_box.signal_connect("button_press_event") do
-# puts "Clicked"
-#end
-
 # initialize boxes and tables
 table = Gtk::Table.new(4, 6, true)
 window.add(table)
 
 #add 12 cards
 (1..4).each do |i|
-  button = Gtk::Button.new("card #{i}")
+  button = Gtk::ToggleButton.new("card #{i}")
   
    #insert 12 cards in the upper left corner
   table.attach_defaults(button, i - 1, i, 0,1)
+
+  button.signal_connect( "toggled" ) { |w| toggle_em(w, button) }
 end
 (5..8).each do |i|
-  button = Gtk::Button.new("card #{i}")
+  button = Gtk::ToggleButton.new("card #{i}")
   
    #insert 12 cards in the upper left corner
   table.attach_defaults(button, i - 5, i - 4, 1,2)
+
+  button.signal_connect( "toggled" ) { |w| toggle_em(w, button) }
 end
 
 (9..12).each do |i|
-  button = Gtk::Button.new("card #{i}")
+  button = Gtk::ToggleButton.new("card #{i}")
   
    #insert 12 cards in the upper left corner
   table.attach_defaults(button, i - 9, i - 8, 2,3)
+
+  button.signal_connect( "toggled" ) { |w| toggle_em(w, button) }
 end
 
 #add Timer button
@@ -64,19 +66,6 @@ end
 
 table.attach_defaults(button,0,6,3,4)
 
-
-#window_box = Gtk::VBox.new(false, 0)
-#window_box.pack_start(table, true, true, 0)
-#window_box.pack_start(button, false, false, 0)
-
-#textview.buffer.insert_pixbuf(iter, catImage)
-
-#window.add(event_box)
-#window.add(window_box)
-# box1 = Gtk::VBox.new(false, 0)
-# window.add(box1)
-# box1.pack_start(button, true, true, 3)
-# box1.pack_start(quit_button, true, true, 0)
 
 window.show_all
 
